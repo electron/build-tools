d853208190a61d3dc5330d71171532b6a0599ccc
Samuel Attard <sattard@slack-corp.com>
2019-08-13 15:18:51 -0700

    fix: allow starting apps from relative paths

--- nix/commands/start.sh
+++ nix/commands/start.sh
@@ -6,10 +6,8 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source $basedir/__load-config.sh
 
-cd $ELECTRON_GN_ROOT/src
-
 if [ "$(uname)" == "Darwin" ]; then
-  "./out/$ELECTRON_OUT_DIR/Electron.app/Contents/MacOS/Electron" "$@"
+  "$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/Electron.app/Contents/MacOS/Electron" "$@"
 else
-  "./out/$ELECTRON_OUT_DIR/electron" "$@"
+  "$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/electron" "$@"
 fi
