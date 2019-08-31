fdeec1ab0f69e68ce03e99cb567d384653a65b73
Shelley Vohr <shelley.vohr@gmail.com>
2019-08-30 21:13:52 -0700

    feat: add command for patch exporting

---
+++ nix/commands/export-patches.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+source $basedir/__load-config.sh
+
+# wrapper fn to check if a key exists in a hashmap
+function array_key_exists() {
+  local _array_name="$1"
+  local _key="$2"
+  local _cmd='echo ${!'$_array_name'[@]}'
+  local _array_keys=($(eval $_cmd))
+  local _key_exists=$(echo " ${_array_keys[@]} " | grep " $_key " &>/dev/null; echo $?)
+  [[ "$_key_exists" = "0" ]] && return 0 || return 1
+}
+
+# init hashmap of valid patch directories
+declare -A patch_dirs
+patch_dirs["node"]="third_party/electron_node"
+patch_dirs["v8"]="v8"
+patch_dirs["chromium"]=""
+patch_dirs["boringssl"]="third_party/boringssl"
+
+SRC_DIR=$ELECTRON_GN_ROOT/src
+
+if [[ "$(array_key_exists 'patch_dirs' "$1"; echo $?)" = "0" ]]; then
+  cd $SRC_DIR/${patch_dirs[$@]}
+  $SRC_DIR/electron/script/git-export-patches -o $SRC_DIR/electron/patches/"$1"
+else
+  echo "Error: invalid patch directory passed."
+fi
