9c9de12083b444a841d30019fe04c172a08621b1
Charles Kerr <ckerr@github.com>
2019-09-16 11:44:34 -0400

    refactor: spawning uname is overkill; use $OSTYPE

--- nix/commands/__load-config.sh
+++ nix/commands/__load-config.sh
@@ -4,16 +4,16 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 config_path="$basedir/../../generated.env.sh"
 
-if [ ! -f "$config_path" ]; then
+if [[ ! -f "$config_path" ]]; then
   echo You configuration has not been generated, please run \"generate-config\"
   exit 1
 fi
 
 source $config_path
 
-if [ "$(uname)" == "Darwin" ]; then
-  ELECTRON_EXEC="$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/Electron.app/Contents/MacOS/Electron"
-else
+if [[ "$OSTYPE" = "linux-gnu" ]]; then
   ELECTRON_EXEC="$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/electron"
+else
+  ELECTRON_EXEC="$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/Electron.app/Contents/MacOS/Electron"
 fi
 
--- nix/commands/debug.sh
+++ nix/commands/debug.sh
@@ -6,8 +6,8 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source $basedir/__load-config.sh
 
-if [ "$(uname)" == "Darwin" ]; then
-  lldb "$ELECTRON_EXEC"
-else
+if [[ "$OSTYPE" = "linux-gnu" ]]; then
   gdb "$ELECTRON_EXEC" -q -ex "source $ELECTRON_GN_ROOT/src/tools/gdb/gdbinit" $@
+else
+  lldb "$ELECTRON_EXEC"
 fi
