20953168d79d9ac15230ac4fe3794d4d92d09ea9
Charles Kerr <ckerr@github.com>
2019-09-16 13:38:08 -0400

    fix: `e pr` uses open or xdg-open depending on OS

--- nix/commands/pr.sh
+++ nix/commands/pr.sh
@@ -9,5 +9,10 @@ cd "$ELECTRON_GN_ROOT/src/electron"
 
 base=$(node "$basedir/../../common/guess-pr-target")
 head=$(git rev-parse --abbrev-ref HEAD)
+url="https://github.com/electron/electron/compare/$base...$head?expand=1"
 
-open "https://github.com/electron/electron/compare/$base...$head?expand=1"
+if [[ "$OSTYPE" = "linux-gnu" ]]; then
+  xdg-open "$url"
+else
+  open "$url"
+fi
