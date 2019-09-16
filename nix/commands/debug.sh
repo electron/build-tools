b0d7c4041ffb9cc8814a024b446a022c11de69d9
Charles Kerr <ckerr@github.com>
2019-09-16 11:18:36 -0400

    feat: when using gdb, load chromium's gdbinit file

--- nix/commands/debug.sh
+++ nix/commands/debug.sh
@@ -9,5 +9,5 @@ source $basedir/__load-config.sh
 if [ "$(uname)" == "Darwin" ]; then
   lldb "$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/Electron.app/Contents/MacOS/Electron"
 else
-  gdb "$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/electron"
+  gdb "$ELECTRON_GN_ROOT/src/out/$ELECTRON_OUT_DIR/electron" -q -ex "source $ELECTRON_GN_ROOT/src/tools/gdb/gdbinit" $@
 fi
