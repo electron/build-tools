64a6f12ff73f444dd7b180b980a0485393b064e8
Charles Kerr <ckerr@github.com>
2019-09-17 10:39:12 -0500

    feat: on nix, add chalklike color-coded messages

--- nix/commands/sync.sh
+++ nix/commands/sync.sh
@@ -3,22 +3,23 @@
 set -e
 
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source "$basedir/__constants.sh"
 source "$basedir/__load-config.sh"
 
-echo "Running '$(which gclient) sync' in '$ELECTRON_GN_ROOT/src'"
-mkdir -p "$ELECTRON_GN_ROOT/src"
-cd "$ELECTRON_GN_ROOT/src"
-
+readonly src_path="$ELECTRON_GN_ROOT/src"
+echo -e "Running '${COLOR_CMD}gclient sync${COLOR_OFF}' in '${COLOR_DIR}$src_path${COLOR_OFF}'"
+mkdir -p "$src_path"
+cd "$src_path"
 gclient sync --with_branch_heads --with_tags "$@"
 
-echo Updating Git Remotes
+echo -e "${COLOR_OK}Updating Git Remotes${COLOR_OFF}"
 
-cd "$ELECTRON_GN_ROOT/src/electron"
+cd "$src_path/electron"
 git remote set-url origin "$ELECTRON_GIT_ORIGIN"
 git remote set-url origin --push "$ELECTRON_GIT_ORIGIN"
 
-cd "$ELECTRON_GN_ROOT/src/third_party/electron_node"
+cd "$src_path/third_party/electron_node"
 git remote set-url origin "$NODE_GIT_ORIGIN"
 git remote set-url origin --push "$NODE_GIT_ORIGIN"
 
-echo Done Syncing
+echo -e "${COLOR_OK}Done Syncing${COLOR_OFF}"
