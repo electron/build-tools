3b713f4eca13bf83568aae23e68c22e2265abaad
Shelley Vohr <shelley.vohr@gmail.com>
2019-09-23 15:47:15 -0700

    chore: add shellcheck linting

--- nix/commands/__tools.sh
+++ nix/commands/__tools.sh
@@ -2,15 +2,19 @@
 
 # constants
 
-declare -r COLOR_CMD='\033[0;33m' # yellow
-declare -r COLOR_DIR='\033[0;36m' # cyan -- same as chalk use in common/
-declare -r COLOR_ERR='\033[0;31m' # red
-declare -r COLOR_OFF='\033[0m' # no-color
-declare -r COLOR_OK='\033[0;32m' # green
-declare -r COLOR_WARN='\033[1;31m' # light red
+declare -r -x COLOR_CMD='\033[0;33m' # yellow
+declare -r -x COLOR_DIR='\033[0;36m' # cyan -- same as chalk use in common/
+declare -r -x COLOR_ERR='\033[0;31m' # red
+declare -r -x COLOR_OFF='\033[0m' # no-color
+declare -r -x COLOR_OK='\033[0;32m' # green
+declare -r -x COLOR_WARN='\033[1;31m' # light red
 
-declare __basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-declare -r ELECTRON_GN_SCRIPTS_ROOT="$(git -C "${__basedir}" rev-parse --show-toplevel)"
+declare __basedir
+__basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+ELECTRON_GN_SCRIPTS_ROOT="$(git -C "${__basedir}" rev-parse --show-toplevel)"
+declare -r ELECTRON_GN_SCRIPTS_ROOT
+
 declare -r DEPOT_TOOLS_PATH="${ELECTRON_GN_SCRIPTS_ROOT}/third_party/depot_tools"
 unset __basedir
 
@@ -20,16 +24,16 @@ ensure_depot_tools() {
   # if it's missing, install it
   if [[ ! -d "$DEPOT_TOOLS_PATH" ]]; then
     echo -e "\n\nCloning ${COLOR_CMD}depot_tools${COLOR_OFF} into '${COLOR_DIR}$DEPOT_TOOLS_PATH${COLOR_OFF}'"
-    git clone -q 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' "$DEPOT_TOOLS_PATH"
-    if [[ $? -ne 0 ]]; then
-      exit $?
+    if ! git clone -q 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' "$DEPOT_TOOLS_PATH"; then
+      echo "Failed to clone depot_tools"
+      exit 1
     fi
   fi
 
   # if it's been awhile, update it
   local -r mtime_age_days="$(perl -e 'print int -M $ARGV[0]' "$DEPOT_TOOLS_PATH")"
   local -r update_interval_days=14
-  if (( $mtime_age_days > $update_interval_days )); then
+  if (( mtime_age_days > update_interval_days )); then
     echo -e "\n\nUpdating ${COLOR_CMD}depot_tools${COLOR_OFF} into '${COLOR_DIR}$DEPOT_TOOLS_PATH${COLOR_OFF}'"
     git -C "${DEPOT_TOOLS_PATH}" pull origin master
     touch "${DEPOT_TOOLS_PATH}"
@@ -40,9 +44,9 @@ ensure_node_modules() {
   # if it's missing, install it
   if [[ ! -d "${ELECTRON_GN_SCRIPTS_ROOT}/node_modules" ]]; then
     echo -e "\n\nRunning '${COLOR_CMD}yarn install${COLOR_OFF}' in '${COLOR_DIR}${ELECTRON_GN_SCRIPTS_ROOT}${COLOR_OFF}'"
-    npx yarn --cwd "${ELECTRON_GN_SCRIPTS_ROOT}" install --frozen-lockfile
-    if [[ $? -ne 0 ]]; then
-      exit $?
+    if ! npx yarn --cwd "${ELECTRON_GN_SCRIPTS_ROOT}" install --frozen-lockfile; then
+      echo "Failed to insall node modules"
+      exit 1
     fi
   fi
 }
--- nix/commands/export-patches.sh
+++ nix/commands/export-patches.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2086,SC2124
 
 set -e
 
@@ -9,9 +10,13 @@ source "$basedir/__load-env.sh"
 function array_key_exists() {
   local _array_name="$1"
   local _key="$2"
-  local _cmd='echo ${!'$_array_name'[@]}'
-  local _array_keys=($(eval $_cmd))
-  local _key_exists=$(echo " ${_array_keys[@]} " | grep " $_key " &>/dev/null; echo $?)
+  local _cmd="echo ${!"$_array_name"[@]}"
+  local _array_keys
+  mapfile -t _array_keys < <(eval $_cmd)
+
+  local _key_exists
+  _key_exists=$(echo "${_array_keys[@]}" | grep " $_key " &>/dev/null; echo $?)
+
   [[ "$_key_exists" = "0" ]] && return 0 || return 1
 }
 
@@ -28,5 +33,5 @@ if [[ "$(array_key_exists 'patch_dirs' "$1"; echo $?)" = "0" ]]; then
   cd "$SRC_DIR/${patch_dirs[$@]}"
   "$SRC_DIR/electron/script/git-export-patches" -o "$SRC_DIR/electron/patches/$1"
 else
-  echo "Error: invalid patch directory passed."
+  echo "Error: $1 is not a valid patch directory."
 fi
