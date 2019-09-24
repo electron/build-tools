a5a5dda9dc8d103d916dde28b0f90b1aa3ce05f8
Shelley Vohr <shelley.vohr@gmail.com>
2019-09-24 19:35:51 +0100

    address changes from review

--- nix/commands/__tools.sh
+++ nix/commands/__tools.sh
@@ -2,12 +2,12 @@
 
 # constants
 
-declare -r -x COLOR_CMD='\033[0;33m' # yellow
-declare -r -x COLOR_DIR='\033[0;36m' # cyan -- same as chalk use in common/
-declare -r -x COLOR_ERR='\033[0;31m' # red
-declare -r -x COLOR_OFF='\033[0m' # no-color
-declare -r -x COLOR_OK='\033[0;32m' # green
-declare -r -x COLOR_WARN='\033[1;31m' # light red
+declare -r COLOR_CMD='\033[0;33m' # yellow
+declare -r COLOR_DIR='\033[0;36m' # cyan -- same as chalk use in common/
+declare -r COLOR_ERR='\033[0;31m' # red
+declare -r COLOR_OFF='\033[0m' # no-color
+declare -r COLOR_OK='\033[0;32m' # green
+declare -r COLOR_WARN='\033[1;31m' # light red
 
 declare __basedir
 __basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -25,7 +25,7 @@ ensure_depot_tools() {
   if [[ ! -d "$DEPOT_TOOLS_PATH" ]]; then
     echo -e "\n\nCloning ${COLOR_CMD}depot_tools${COLOR_OFF} into '${COLOR_DIR}$DEPOT_TOOLS_PATH${COLOR_OFF}'"
     if ! git clone -q 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' "$DEPOT_TOOLS_PATH"; then
-      echo "Failed to clone depot_tools"
+      echo -e "${COLOR_ERR}Failed to clone depot_tools!${COLOR_OFF}"
       exit 1
     fi
   fi
@@ -45,7 +45,7 @@ ensure_node_modules() {
   if [[ ! -d "${ELECTRON_GN_SCRIPTS_ROOT}/node_modules" ]]; then
     echo -e "\n\nRunning '${COLOR_CMD}yarn install${COLOR_OFF}' in '${COLOR_DIR}${ELECTRON_GN_SCRIPTS_ROOT}${COLOR_OFF}'"
     if ! npx yarn --cwd "${ELECTRON_GN_SCRIPTS_ROOT}" install --frozen-lockfile; then
-      echo "Failed to insall node modules"
+      echo -e "${COLOR_ERR}Failed to install node modules!${COLOR_OFF}"
       exit 1
     fi
   fi
