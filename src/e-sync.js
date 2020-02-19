88853676e2ec366501f8ac34a0108ad3f4ba6d54
Samuel Attard <sattard@slack-corp.com>
2020-02-19 14:47:18 -0800

    Use an electron provided windows toolchain

--- src/e-sync.js
+++ src/e-sync.js
@@ -31,7 +31,16 @@ function runGClientSync(config, syncArgs) {
 
   const exec = 'python';
   const args = ['gclient.py', 'sync', '--with_branch_heads', '--with_tags', ...syncArgs];
-  const opts = { cwd: srcdir };
+  const opts = {
+    cwd: srcdir,
+    env: {
+      DEPOT_TOOLS_WIN_TOOLCHAIN: '1',
+      DEPOT_TOOLS_WIN_TOOLCHAIN_BASE_URL:
+        'https://electron-build-tools.s3-us-west-2.amazonaws.com/win32/toolchains/_',
+      GYP_MSVS_HASH_9ff60e43ba91947baca460d0ca3b1b980c3a2c23:
+        '6d205e765a23d3cbe0fcc8d1191ae406d8bf9c04',
+    },
+  };
   depot.execFileSync(config, exec, args, opts);
   setOrigin(path.resolve(srcdir, 'electron'), config.origin.electron);
   setOrigin(path.resolve(srcdir, 'third_party', 'electron_node'), config.origin.node);
