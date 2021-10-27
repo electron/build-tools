1cae74fda968b52ff13c4a070c0a5ff5d4dc7208
Cheng Zhao <zcbenz@gmail.com>
2021-10-27 20:25:08 +0900

    fix: sysctl.proc_translated does not exist on x64 macOS

--- src/utils/arm.js
+++ src/utils/arm.js
@@ -1,12 +1,16 @@
 const cp = require('child_process');
 
 const getIsArm = () => {
-  const isCurrentlyTranslated = cp.execSync('sysctl sysctl.proc_translated');
+  if (process.arch === 'arm64') {
+    return true;
+  }
 
-  return (
-    process.arch === 'arm64' ||
-    isCurrentlyTranslated.toString().startsWith('sysctl.proc_translated: 1')
-  );
+  try {
+    const isCurrentlyTranslated = cp.execSync('sysctl sysctl.proc_translated', { stdio: 'pipe' });
+    return isCurrentlyTranslated.toString().startsWith('sysctl.proc_translated: 1');
+  } catch (e) {
+    return false;
+  }
 };
 
 module.exports = {
