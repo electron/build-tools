1684dbef329ba46077ee8a1ce77226e71ac509b8
Shelley Vohr <shelley.vohr@gmail.com>
2021-10-27 14:48:29 +0200

    fix: error handling in ARM check (#308)

--- src/utils/arm.js
+++ src/utils/arm.js
@@ -1,12 +1,19 @@
 const cp = require('child_process');
 
+// See https://developer.apple.com/documentation/apple-silicon/about-the-rosetta-translation-environment.
 const getIsArm = () => {
-  const isCurrentlyTranslated = cp.execSync('sysctl sysctl.proc_translated');
+  try {
+    const isCurrentlyTranslated = cp.execSync('sysctl sysctl.proc_translated', { stdio: 'pipe' });
 
-  return (
-    process.arch === 'arm64' ||
-    isCurrentlyTranslated.toString().startsWith('sysctl.proc_translated: 1')
-  );
+    return (
+      process.arch === 'arm64' ||
+      isCurrentlyTranslated.toString().startsWith('sysctl.proc_translated: 1')
+    );
+  } catch (e) {
+    // On non-ARM macs `sysctl sysctl.proc_translated` throws with
+    // sysctl: unknown oid 'sysctl.proc_translated'
+    return false;
+  }
 };
 
 module.exports = {
