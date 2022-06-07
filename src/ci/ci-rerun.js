cffcdd9eb74cc4b94aa3464884cbb6be957296d3
Shelley Vohr <shelley.vohr@gmail.com>
2022-06-07 13:45:20 +0200

    chore: correct description

--- src/ci/ci-rerun.js
+++ src/ci/ci-rerun.js
@@ -7,7 +7,7 @@ const got = require('got');
 const { fatal } = require('../utils/logging');
 
 program
-  .description('Show information about CI job statuses')
+  .description('Rerun CI workflows')
   .argument('[workflow]', 'The ID of the workflow to rerun')
   .option('-f, --from-failed', 'Rerun workflow from failed', false)
   .option('-e, --enable-ssh', 'Rerun workflow from failed', false)
