50658625a60832ca130843ff6f30da183311b9da
David Sanders <dsanders11@ucsbalum.com>
2022-10-09 21:32:32 -0700

    chore: add missing require

--- tests/e-init-spec.js
+++ tests/e-init-spec.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const ajv = require('ajv');
+
 const createSandbox = require('./sandbox');
 const schema = require('../evm-config.schema.json');
 
