1598443efbf43a7c9b0fc1d6ad1e65e77a9020f6
Shelley Vohr <shelley.vohr@gmail.com>
2022-11-22 10:00:44 +0100

    fix: allow overriding DEVELOPER_DIR

--- src/e-load-xcode.js
+++ src/e-load-xcode.js
@@ -18,12 +18,17 @@ if (Xcode.ensureXcode() === false) {
 // Select our new xcode
 const output = childProcess.execFileSync('xcode-select', ['-p']).toString();
 if (!output.trim().startsWith(Xcode.XcodePath)) {
-  console.info(
-    `Setting your Xcode installation to ${color.path(Xcode.XcodePath)}, this will require sudo`,
-  );
-  childProcess.execFileSync('sudo', ['xcode-select', '-s', Xcode.XcodePath], {
-    stdio: 'inherit',
-  });
+  if (process.env.DEVELOPER_DIR.length) {
+    console.info(`${color.info} Using overridden Xcode at ${process.env.DEVELOPER_DIR}`);
+    return;
+  } else {
+    console.info(
+      `Setting your Xcode installation to ${color.path(Xcode.XcodePath)}, this will require sudo`,
+    );
+    childProcess.execFileSync('sudo', ['xcode-select', '-s', Xcode.XcodePath], {
+      stdio: 'inherit',
+    });
+  }
 }
 
 // Ensure that we have accepted the Xcode license agreement
