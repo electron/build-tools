7596f352bca4664d897538ec6549351973233d0d
John Kleinschmidt <jkleinsc@electronjs.org>
2023-08-29 17:20:23 -0400

    apply review suggestions
    Co-Authored-By: David Sanders <dsanders11@ucsbalum.com>

--- src/e-build.js
+++ src/e-build.js
@@ -123,17 +123,13 @@ program
       } catch (ex) {
         if (target === targets['node:headers']) {
           // Older versions of electron use a different target for node headers so try that if the new one fails.
-          try {
-            const olderTarget = 'third_party/electron_node:headers';
-            console.info(
-              `${color.info} Error building ${target}; trying older ${olderTarget} target`,
-            );
-            runNinja(config, olderTarget, options.goma, ninjaArgs);
-          } catch (ex) {
-            tnrow(ex);
-          }
+          const olderTarget = 'third_party/electron_node:headers';
+          console.info(
+            `${color.info} Error building ${target}; trying older ${olderTarget} target`,
+          );
+          runNinja(config, olderTarget, options.goma, ninjaArgs);
         } else {
-          tnrow(ex);
+          throw ex;
         }
       }
     } catch (e) {
