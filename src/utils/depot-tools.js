electron/build-tools

commit 700187712658e6fbdf521aac1a3ca6e8de8f0f2d

chore: log info message when auto-update disabled

src/e
@@ -19,6 +19,7 @@ function maybeCheckForUpdates() {
   // skip auto-update check if disabled
   const disableAutoUpdatesFile = path.resolve(__dirname, '..', '.disable-auto-updates');
   if (fs.existsSync(disableAutoUpdatesFile)) {
+    console.info(`${color.info} Auto-updates disabled, skipping check for updates`);
     return;
   }
   // don't check if we already checked recently

commit 244747e2899739b63f810e6fc91d74e1d0388e24

fix: Send "Auto-updates disabled" message to stderr, not stdout
Outputting to stdout breaks commands like `` cd `e show exec` ``, so
use stderr instead.

Link: https://github.com/electron/build-tools/pull/527
Fixes: 700187712658e6fbdf521aac1a3ca6e8de8f0f2d ("chore: log info message when auto-update disabled")
Fixes: 1572597cce0fea510672008d0103fc0001058921 ("chore: log info message when depot_tools auto-update disabled")
Suggested-by: David Sanders <dsanders11@ucsbalum.com>
Signed-off-by: Pascal Ernster <git@hardfalcon.net>

src/e
@@ -17,9 +17,13 @@ refreshPathVariable();
 
 function maybeCheckForUpdates() {
   // skip auto-update check if disabled
+  //
+  // NB: send updater's stdout to stderr so its log messages are visible
+  // but don't pollute stdout. For example, calling `FOO="$(e show exec)"`
+  // should not get a FOO that includes "Checking for build-tools updates".
   const disableAutoUpdatesFile = path.resolve(__dirname, '..', '.disable-auto-updates');
   if (fs.existsSync(disableAutoUpdatesFile)) {
-    console.info(`${color.info} Auto-updates disabled, skipping check for updates`);
+    console.error(`${color.info} Auto-updates disabled, skipping check for updates`);
     return;
   }
   // don't check if we already checked recently

src/utils/depot-tools.js
@@ -33,7 +33,10 @@ function ensureDepotTools() {
   }
 
   if (fs.existsSync(markerFilePath)) {
-    console.info(
+    // NB: send updater's stdout to stderr so its log messages are visible
+    // but don't pollute stdout. For example, calling `FOO="$(e show exec)"`
+    // should not get a FOO that includes "Checking for build-tools updates".
+    console.error(
       `${color.info} Automatic depot_tools updates disabled, skipping check for updates`,
     );
     return;