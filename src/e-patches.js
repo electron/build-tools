7cfa0c48eb0bb7ef7b2b32f990719e2d500b0fe7
Charles Kerr <charles@charleskerr.com>
2024-01-30 13:49:31 -0600

    feat: support array-of-objects patches config file format
    feat: support filtering exported patches by keyword
    
    fix: oops

--- src/e-patches.js
+++ src/e-patches.js
@@ -13,7 +13,11 @@ program
   .description(
     "Refresh all patches if 'all' is specified; otherwise, refresh patches in $root/src/electron/patches/$target",
   )
-  .option('-c, --config <filename>', "Specify a config file", path.resolve(evmConfig.current().root, 'src', 'electron', 'patches', 'config.json'))
+  .option(
+    '-c, --config <filename>',
+    'Specify a config file',
+    path.resolve(evmConfig.current().root, 'src', 'electron', 'patches', 'config.json'),
+  )
   .option('--list-targets', 'Show all supported patch targets', false)
   .action((target, options) => {
     try {
@@ -23,8 +27,12 @@ program
       // build the list of targets
       const targets = {};
       const patchesConfig = options.config;
-      for (const [key, val] of Object.entries(JSON.parse(fs.readFileSync(patchesConfig)))) {
-        targets[path.basename(key)] = val;
+      const configData = JSON.parse(fs.readFileSync(patchesConfig));
+      if (Array.isArray(configData)) {
+        for (const target of configData) targets[path.basename(target.patch_dir)] = target;
+      } else if (typeof configData == 'object') {
+        for (const [patch_dir, repo] of Object.entries(configData))
+          targets[path.basename(patch_dir)] = { patch_dir, repo };
       }
 
       if (options.listTargets) {
@@ -46,13 +54,16 @@ program
           encoding: 'utf8',
         });
       } else if (targets[target]) {
+        const targetConfig = targets[target];
         const script = path.resolve(srcdir, 'electron', 'script', 'git-export-patches');
-        depot.execFileSync(
-          config,
-          'python3',
-          [script, '-o', path.resolve(srcdir, 'electron', 'patches', target)],
-          { cwd: path.resolve(config.root, targets[target]), stdio: 'inherit', encoding: 'utf8' },
-        );
+        const opts = {
+          cwd: path.resolve(config.root, targetConfig.repo),
+          stdio: 'inherit',
+          encoding: 'utf8',
+        };
+        const args = [script, '--output', path.resolve(config.root, targetConfig.patch_dir)];
+        if (targetConfig.grep) args.push('--grep', targetConfig.grep);
+        depot.execFileSync(config, 'python3', args, opts);
       } else {
         console.log(`${color.err} Unrecognized target ${color.cmd(target)}.`);
         console.log(
