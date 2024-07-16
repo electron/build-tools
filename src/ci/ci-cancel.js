6ac782f96fcd72919809249079e2835f9f1c33ba
Shelley Vohr <shelley.vohr@gmail.com>
2024-07-16 11:27:07 +0200

    chore: remove CircleCI logic (#621)

--- src/ci/ci-cancel.js
+++ src/ci/ci-cancel.js
@@ -5,9 +5,9 @@ const { program, InvalidArgumentError } = require('commander');
 const got = require('got');
 
 const { fatal } = require('../utils/logging');
-const { archOption, ArchTypes, BuildTypes, getCIType } = require('./common');
+const { archOption, ArchTypes, BuildTypes } = require('./common');
 
-const { CIRCLE_TOKEN, APPVEYOR_CLOUD_TOKEN } = process.env;
+const { APPVEYOR_CLOUD_TOKEN } = process.env;
 
 const cancelAppveyorBuild = async id => {
   const { statusCode } = await got
@@ -23,56 +23,19 @@ const cancelAppveyorBuild = async id => {
   console.info(`${chalk.white(`${msg} Appveyor ${ArchTypes[arch]} build with id ${id}`)}`);
 };
 
-const cancelCircleCIWorkflow = async id => {
-  const { stopped_at } = await got(`https://circleci.com/api/v2/workflow/${id}`, {
-    username: CIRCLE_TOKEN,
-    password: '',
-  }).json();
-
-  // Don't try to cancel completed builds.
-  if (stopped_at !== null) {
-    console.log(`${chalk.white(`${msg} Could not cancel completed CircleCI build with id ${id}`)}`);
-    return;
-  }
-
-  const { message } = await got
-    .post(`https://circleci.com/api/v2/workflow/${id}/cancel`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      username: CIRCLE_TOKEN,
-      password: '',
-    })
-    .json();
-
-  const msg = message === 'Accepted.' ? 'Successfully cancelled' : 'Failed to cancel';
-  console.log(`${chalk.white(`${msg} CircleCI workflow with id ${id}`)}`);
-};
-
 program
   .description('Cancel CI workflows and builds')
   .argument('<id>', 'The ID of the workflow or build to cancel')
   .addOption(archOption)
   .action(async (id, { arch }) => {
     try {
-      const type = getCIType(id);
-
-      if (type === BuildTypes.CIRCLECI) {
-        if (!CIRCLE_TOKEN) {
-          fatal('process.env.CIRCLE_TOKEN required for CircleCI cancellations');
-        }
-
-        await cancelCircleCIWorkflow(id);
-      } else if (type === BuildTypes.APPVEYOR) {
-        if (!arch) {
-          throw new InvalidArgumentError('arch is required for Appveyor cancellations');
-        } else if (!APPVEYOR_CLOUD_TOKEN) {
-          fatal('process.env.APPVEYOR_CLOUD_TOKEN required for AppVeyor cancellations');
-        }
-
-        await cancelAppveyorBuild(id, arch);
+      if (!arch) {
+        throw new InvalidArgumentError('arch is required for Appveyor cancellations');
+      } else if (!APPVEYOR_CLOUD_TOKEN) {
+        fatal('process.env.APPVEYOR_CLOUD_TOKEN required for AppVeyor cancellations');
       }
+
+      await cancelAppveyorBuild(id, arch);
     } catch (e) {
       fatal(e.message);
     }
