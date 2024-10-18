4cc5f72fe7524c10826ecf6dffd603d80aff6c2c
David Sanders <dsanders11@ucsbalum.com>
2024-10-18 12:40:58 -0700

    refactor: replace got usage with builtin fetch

--- src/ci/ci-cancel.js
+++ src/ci/ci-cancel.js
@@ -2,7 +2,6 @@
 
 const { default: chalk } = require('chalk');
 const { program, InvalidArgumentError } = require('commander');
-const got = require('got');
 
 const { fatal } = require('../utils/logging');
 const { archOption, ArchTypes, BuildTypes } = require('./common');
@@ -10,14 +9,16 @@ const { archOption, ArchTypes, BuildTypes } = require('./common');
 const { APPVEYOR_CLOUD_TOKEN } = process.env;
 
 const cancelAppveyorBuild = async (id) => {
-  const { statusCode } = await got
-    .delete(`https://ci.appveyor.com/api/builds/electron-bot/${ArchTypes[arch]}/${id}`, {
+  const { statusCode } = await fetch(
+    `https://ci.appveyor.com/api/builds/electron-bot/${ArchTypes[arch]}/${id}`,
+    {
+      method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${APPVEYOR_CLOUD_TOKEN}`,
       },
-    })
-    .json();
+    },
+  ).then((resp) => resp.json());
 
   const msg = statusCode === 204 ? 'Successfully cancelled' : 'Failed to cancel';
   console.info(`${chalk.white(`${msg} Appveyor ${ArchTypes[arch]} build with id ${id}`)}`);
