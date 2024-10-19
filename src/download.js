714ee8e616bb2e0184039645dbd93e93674dfede
David Sanders <dsanders11@ucsbalum.com>
2024-10-19 09:28:23 -0700

    refactor: replace got usage with builtin fetch (#667)

--- src/download.js
+++ src/download.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const got = require('got');
 const stream = require('stream');
 const { pipeline } = require('stream/promises');
 const ProgressBar = require('progress');
@@ -8,20 +7,17 @@ const { fatal } = require('./utils/logging');
 
 const MB_BYTES = 1024 * 1024;
 
-const progressStream = function (tokens) {
+const progressStream = function (total, tokens) {
   var pt = new stream.PassThrough();
 
   pt.on('pipe', function (stream) {
-    stream.on('response', function (res) {
-      const total = parseInt(res.headers['content-length'], 10);
-      const bar = new ProgressBar(tokens, { total: Math.round(total) });
+    const bar = new ProgressBar(tokens, { total: Math.round(total) });
 
-      pt.on('data', function (chunk) {
-        const elapsed = new Date() - bar.start;
-        const rate = bar.curr / (elapsed / 1000);
-        bar.tick(chunk.length, {
-          mbRate: (rate / MB_BYTES).toFixed(2),
-        });
+    pt.on('data', function (chunk) {
+      const elapsed = new Date() - bar.start;
+      const rate = bar.curr / (elapsed / 1000);
+      bar.tick(chunk.length, {
+        mbRate: (rate / MB_BYTES).toFixed(2),
       });
     });
   });
@@ -29,12 +25,15 @@ const progressStream = function (tokens) {
   return pt;
 };
 
-const progress = progressStream('[:bar] :mbRateMB/s :percent :etas');
 const write = fs.createWriteStream(process.argv[3]);
 
-function tryDownload(attemptsLeft = 3) {
-  pipeline(
-    got.default.stream(process.argv[2]),
+async function tryDownload(attemptsLeft = 3) {
+  const response = await fetch(process.argv[2]);
+  const total = parseInt(response.headers.get('content-length'), 10);
+  const progress = progressStream(total, '[:bar] :mbRateMB/s :percent :etas');
+
+  await pipeline(
+    stream.Readable.fromWeb(response.body),
     ...(process.env.CI ? [write] : [progress, write]),
   ).catch((err) => {
     if (attemptsLeft === 0) {
