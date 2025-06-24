eafcef25ea71443e90fc69680074590cd980bd06
Samuel Attard <samuel.r.attard@gmail.com>
2025-06-23 22:31:07 -0700

    feat: also use electron VC'ed main.star

--- src/utils/siso.js
+++ src/utils/siso.js
@@ -30,7 +30,7 @@ function sisoFlags(config) {
     '-reapi_address',
     reclient.serviceAddress(config),
     '-load',
-    path.resolve(__dirname, '../siso/config/electron.star'),
+    path.resolve(config.root, 'electron/build/siso/main.star'),
   ];
 }
 
