274cba0474f0d1e4e6adbb66c1da48556cb0add5
John Kleinschmidt <jkleinsc@electronjs.org>
2025-07-02 12:02:06 -0400

    chore: update rbe credential helper to 0.5.1 (#737)
    (cherry picked from commit e4fae9b3aa083c6d07ad14fff3ed333a094f6d63)

--- src/utils/reclient.js
+++ src/utils/reclient.js
@@ -14,7 +14,7 @@ const rbeHelperPath = path.resolve(
 );
 const RBE_SERVICE_ADDRESS = 'rbe.notgoma.com:443';
 
-const CREDENTIAL_HELPER_TAG = 'v0.5.0';
+const CREDENTIAL_HELPER_TAG = 'v0.5.1';
 
 let usingRemote = true;
 
